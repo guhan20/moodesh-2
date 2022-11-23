aaaa723d34c05954d8f7ff5af26cb10c3f914c91
GT ɮɛʀռօ <78096979+GTBERNO@users.noreply.github.com>
2022-11-23 19:55:44 +0400

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -564,7 +564,7 @@ If you do not see the requested movie / series file, look at the next page
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
-            reply_markup=reply_markup
+            reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )           
     elif query.data == "owner":
