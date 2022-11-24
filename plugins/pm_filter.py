1995577034c35da6652f8811e7b2b6de43dcb96e
GT ɮɛʀռօ <78096979+GTBERNO@users.noreply.github.com>
2022-11-24 09:13:02 +0400

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -44,7 +44,7 @@ async def give_filter(client, message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer(UNAUTHORIZED_CALLBACK_TEXT, show_alert=True)
+        return await query.answer((f"മോനെ {query.from_user.first_name} ഇത്‌ നിനക്കുള്ളതല്ല 🤭\n\n {query.message.reply_to_message.from_user.first_name} ന്റെ റിക്യുസ്റ്റ് ആണ് ഇത് 🙂\n\nRequest your own 🥰\n\n© Cinimaadholokam", show_alert=True)
     try:
         offset = int(offset)
     except:
