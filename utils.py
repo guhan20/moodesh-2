17cbb335e3afad418ece1c8210691dd45edf7e04
GT ɮɛʀռօ <78096979+GTBERNO@users.noreply.github.com>
2022-11-25 10:35:05 +0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import logging
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
 from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM, redirected_env
 #from imdb import IMDb
-from PyMovieDb import IMDB
+from pyimdb import IMDB
 import asyncio
 from pyrogram.types import Message
 from typing import Union
