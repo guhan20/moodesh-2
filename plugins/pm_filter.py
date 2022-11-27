572cc7413a1c4d053b59ada712d703516982053d
GT ɮɛʀռօ <78096979+GTBERNO@users.noreply.github.com>
2022-11-27 09:30:23 +0400

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -566,7 +566,7 @@ If you do not see the requested movie / series file, look at the next page
            #     )
       #      )
             await query.answer(f'ʜᴇʏ {query.from_user.first_name} ʏᴏᴜʀ ғɪʟᴇ ɪs ʀᴇᴅʏ 😴\n\n𝙥𝙡𝙚𝙖𝙨𝙚 𝙘𝙡𝙞𝙘𝙠 @ 𝙗𝙪𝙩𝙩𝙤𝙣 💡\n\n𝙊𝙪𝙧 S𝙘𝙧𝙤𝙡𝙡 𝘿𝙤𝙬𝙣 ⬇️', show_alert=True)
-            await asyncio.sleep(2) 
+            await asyncio.sleep(300) 
            # await msg1.delete()
             await msg.delete()
             del msg
