b290ab75d59e7a24b1efadfe01f04727b642577b
GT ɮɛʀռօ <78096979+GTBERNO@users.noreply.github.com>
2022-11-27 16:25:18 +0400

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -782,6 +782,7 @@ async def auto_filter(client, msg: pyrogram.types.Message, spoll=False):
      #   else:
       #      return
   #  else:
+    message = msg
     settings = await get_settings(msg.message.chat.id)
     message = msg.message.reply_to_message  # msg will be callback query
     search, files, offset, total_results = spoll
