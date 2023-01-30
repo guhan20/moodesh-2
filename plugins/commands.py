04b87a589773eba96604cc098daab89a84e72ca8
❤️ ഗുഹൻ ❤️ <113303372+guhan20@users.noreply.github.com>
2023-01-30 10:03:25 +0400

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -585,10 +585,10 @@ async def caption(bot, message):
 @Client.on_message(filters.command("pong"))
 async def ping(bot, message):
     start_time = t.time()
-    bot.send_message(chat_id=message.message.chat_id, text="Pong!")
+    bot.send_message(chat_id=message.chat_id, text="Pong!")
     end_time = t.time()
     response_time = (end_time - start_time) * 1000
-    bot.send_message(chat_id=message.message.chat_id, text=f"Response time: {response_time:.0f} ms")    
+    bot.send_message(chat_id=message.chat_id, text=f"Response time: {response_time:.0f} ms")    
     
 @Client.on_message(filters.command("ping"))
 async def ping_pong(bot, message):
