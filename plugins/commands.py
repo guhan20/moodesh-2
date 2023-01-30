4939187c6651d7a3905e7b34632eee830f1c6ddf
❤️ ഗുഹൻ ❤️ <113303372+guhan20@users.noreply.github.com>
2023-01-30 09:10:28 +0400

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -609,6 +609,21 @@ async def ping_pong(bot, message):
     await asyncio.sleep(10)
     await S.delete()
     await M.delete()
+    
+#=====================================================
+BOT_START_TIME = time.time()
+#=====================================================
+
+@Client.on_message(filters.command("pong"))
+async def ping(_, message):
+    start_t = time.time()  
+    avr = await message.reply_text("•••")  
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    uptime = time.strftime("%Hh | %Mm | %Ss", time.gmtime(time.time() - BOT_START_TIME))   
+    await avr.edit(f"-ᴄᴜʀʀᴇɴᴛ ʙᴏᴛ sᴛᴀᴛᴜs-\n\n 🏓 ᴘᴏɴɢ : {time_taken_s:.3f} ms\n\n ⏰ ᴜᴘᴛɪᴍᴇ : {uptime}")
+    await asyncio.sleep(DELETE_TIME)
+    await avr.delete()
 
 @Client.on_message(filters.command('set_template'))
 async def save_template(client, message):
